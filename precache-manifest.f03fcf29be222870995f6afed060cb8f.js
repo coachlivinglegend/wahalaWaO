ee56c86cdb608a6e4c216601d18201f81c9254e6
Daniel Beckley <tomiwabeckley@gmail.com>
2020-07-24 13:59:21 +0100

    Updates

---
+++ precache-manifest.f03fcf29be222870995f6afed060cb8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c894eef0973fbb67c37c9f9f497daa1a",
+    "url": "/wahalaWaO/index.html"
+  },
+  {
+    "revision": "f307572db77064751242",
+    "url": "/wahalaWaO/static/css/main.d93d0046.chunk.css"
+  },
+  {
+    "revision": "ba1f3400366fbcce5afe",
+    "url": "/wahalaWaO/static/js/2.e57fe1d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wahalaWaO/static/js/2.e57fe1d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f307572db77064751242",
+    "url": "/wahalaWaO/static/js/main.85714028.chunk.js"
+  },
+  {
+    "revision": "6e41708a0314a123bedb",
+    "url": "/wahalaWaO/static/js/runtime-main.f8b584c0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wahalaWaO/precache-manifest.7a63db42e5abc80171167382622dfdee.js"
+  "/wahalaWaO/precache-manifest.f03fcf29be222870995f6afed060cb8f.js"
 );
 
 self.addEventListener('message', (event) => {
