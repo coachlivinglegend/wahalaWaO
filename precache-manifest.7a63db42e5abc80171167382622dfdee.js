36f5f08296e6f009a13d5f3dddb1ca65ac6da499
Daniel Beckley <tomiwabeckley@gmail.com>
2020-07-24 13:39:52 +0100

    Updates

---
+++ precache-manifest.7a63db42e5abc80171167382622dfdee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ff1dc40a84fa6912cbc2c5804b51a09",
+    "url": "/wahalaWaO/index.html"
+  },
+  {
+    "revision": "0da15790f164a61cce75",
+    "url": "/wahalaWaO/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "895d31552c51ba2eccbe",
+    "url": "/wahalaWaO/static/js/2.53d0a898.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wahalaWaO/static/js/2.53d0a898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0da15790f164a61cce75",
+    "url": "/wahalaWaO/static/js/main.f2b41198.chunk.js"
+  },
+  {
+    "revision": "6e41708a0314a123bedb",
+    "url": "/wahalaWaO/static/js/runtime-main.f8b584c0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wahalaWaO/static/media/logo.5d5d9eef.svg"
+  }
+]);
